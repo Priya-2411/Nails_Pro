b87d935d78f6a53a51b2e9a8c389c2486c1cdc3d
Priya Patel <200447419@student.georgianc.on.ca>
2021-03-31 11:21:49 -0400

    Add project files.

---
+++ NailsPro/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace NailsPro.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
